6072c58dee6495fa9521a4154832e275c7f12223
Leonardo Oliveira <108847387+leooliveira2@users.noreply.github.com>
2022-10-18 19:55:49 -0300

    removendo autoload manual

--- autoload/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-spl_autoload_register(function(string $caminho) {
-    $caminhoArquivo = str_replace("SisLogin\\Projeto", "src", $caminho);
-    $caminhoArquivo .= ".php";
-    $caminhoArquivo = str_replace('\\', DIRECTORY_SEPARATOR, $caminhoArquivo);
-
-    require_once $caminhoArquivo;
-});
